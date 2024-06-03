b22d941ab5e7c1dda24659a3f3fbc57ff6b55591
Julian <julianser@hotmail.com.ar>
2024-06-03 20:48:35 -0300

    Se instalo Sass, y se realizaron las columnas y tarjetas de Trello

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,136 @@
-
-
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <section className="trello-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+        <header>Backlog</header>
+        <ul>
+          <li><img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="card image" />Design & Research</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Backlog</header>
+        <ul>
+          <li><img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="card image" />Design & Research</li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Backlog</header>
+        <ul>
+          <li><img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="card image" />Design & Research</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Backlog</header>
+        <ul>
+          <li><img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="card image" />Design & Research</li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Backlog</header>
+        <ul>
+          <li><img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="card image" />Design & Research</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Backlog</header>
+        <ul>
+          <li><img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="card image" />Design & Research</li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Backlog</header>
+        <ul>
+          <li><img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="card image" />Design & Research</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+          <li>second</li>
+          <li>third</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Backlog</header>
+        <ul>
+          <li><img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="card image" />Design & Research</li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
